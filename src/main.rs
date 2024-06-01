565f7a64de33f6b6072fe2bd895dedebd81669c3
Khionu Sybiern <dev@khionu.net>
2024-05-31 22:54:22 -0700

    feat: create initial TUI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
+mod tui;
+
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
+    to
 }
---
+++ src/tui/mod.rs
@@ -0,0 +1,10 @@
+use std::{
+  io::{self, Stdout},
+  time::Duration,
+};
+
+use eyre::Result;
+
+pub fn setup() -> Result<Terminal<CrosstermBackend<Stdout>>> {
+  
+}
